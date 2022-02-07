f48ca4838dddea04865370c9349b1e264200df3d
rifqyyusuf <rifqy@importir.co>
2022-02-07 18:19:46 +0800

    membuat kalkulator

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
+var hasil = document.getElementById("hasil");
+
+function fhasil() {
+  hasil.value = Number(input1.value) + Number(input2.value);
+}
